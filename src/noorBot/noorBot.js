58b520825e088d65dafa1581246ff8315ffd562e
ks-randhawa0649 <ksrandhawa0649@gmail.com>
2025-02-28 22:05:54 +0000

    configure react files

---
+++ src/noorBot/noorBot.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ChatBot from "react-chatbotify";
+import {FetchResponse} from './noorBotAI';
+import './noorBot.css';
+
+const settings = {
+    isOpen: true,
+    general: {
+      primaryColor: '#42b0c5',
+      secondaryColor: '#491d8d',
+      fontFamily: 'Arial, sans-serif',
+      embedded: true,
+      showFooter: true,
+      height: "6000px",
+    },
+    header: {
+      title: 'NoorBot'
+    },
+    botBubble: {
+      simStream: true
+    }
+
+  };
+
+const bodyStyle = {
+  display: "flex", 
+  justifyContent: "center",
+  alignItems: "center", 
+  backgroundColor : '#B0E0E6',
+  height:'100vh',
+  width:'100vw',
+}
+
+const runNoorAI = async(prompt) => {
+const response = await FetchResponse(prompt);
+return response;
+}
+
+const flow = {
+  start:{
+    message: "Hi, I'm NoorBot.....what is your query?",
+    path: "model"
+  },
+  model:{
+    message: async(params) => {return await runNoorAI(params.userInput)},
+    path: "model"
+  }
+};
+
+const NoorBot = () => {
+    return(
+    <div style={bodyStyle}>
+        <ChatBot flow = {flow} settings={settings} />
+    </div>
+
+    );
+};  
+
+export default NoorBot;
